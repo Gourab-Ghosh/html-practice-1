1304226ff6d3bba01fbf2f58f6de5a7758d3d62d
Gourab-Ghosh <gourab200ghosh@gmail.com>
2020-11-01 23:54:21 +0530

    seperated common html codes

---
+++ js/include-html.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#load-navbar").load("../common-html-codes/navbar.html");
+});
+
+$(function(){
+    $("#load-contact-and-subscribe").load("../common-html-codes/contact-and-subscribe.html");
+});
